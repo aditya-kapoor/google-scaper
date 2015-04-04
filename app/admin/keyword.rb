73fe95cca9dd3cb8ff4ef9615c6c12b1e89d5441
Aditya Kapoor <adityakapoor.mait@gmail.com>
2015-04-04 19:56:02 +0530

    bootstarp application with ActiveAdmin

---
+++ app/admin/keyword.rb
@@ -0,0 +1,30 @@
+ActiveAdmin.register Keyword do
+  permit_params :name
+
+  collection_action :upload_csv do
+  end
+
+  collection_action :save_csv, method: :post do
+    Keyword.import(params[:csv_file].tempfile)
+    redirect_to admin_keywords_path, notice: 'CSV Uploaded successfully.'
+  end
+
+  action_item only: :index do
+    link_to('Upload CSV', upload_csv_admin_keywords_path)
+  end
+
+  index do
+    id_column
+    column :name
+    actions
+  end
+
+  filter :name
+
+  form do |f|
+    f.inputs "Keyword Details" do
+      f.input :name
+    end
+    f.actions
+  end
+end
